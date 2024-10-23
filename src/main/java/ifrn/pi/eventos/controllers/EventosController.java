07d8532da6767bc8dd1fd4ed558cf7415018e630
ADEILTON PIRES FERNANDES FILHO <20211204010043@ifrn.local>
2024-10-23 14:14:13 -0300

    Ativiade 302

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,7 +1,10 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -9,6 +12,17 @@ public class EventosController {
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@RequestMapping("/eventos/form/executado")
+	public String executado(Evento evento) {
+		System.out.println("Formulário enviado!");
+		System.out.println("*Dados enviados*");
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+		return "executado";
 	}
 	
 }
--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -11,4 +11,5 @@ public class IndexController {
 		System.out.println("Chamou o método index");
 		return "home";
 	}
+
 }
